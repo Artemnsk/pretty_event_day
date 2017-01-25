27e34dff70d9e282a74907490d9554ac7179fb5d
Artem Shelkov <shelkov1991@gmail.com>
2017-01-25 15:39:47 +0700

    Initial commit - took files from f2f site.

---
+++ pretty_event_day.api.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Custom alteration of available day options.
+ */
+function hook_pretty_event_day_options_alter(&$available_options, $entity, $type) {
+  // Some actions with $available_options.
+}
